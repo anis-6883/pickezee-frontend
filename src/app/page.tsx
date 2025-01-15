ece1f2c1baf7be05b35ce7f79b9b6008b170dfaa
anis-6883 <anisseam238@gmail.com>
2025-01-15 22:00:37 +0600

    ci: project initial setup with ui.shadcn & next-theme

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from "@/components/common/ModeToggle";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <ModeToggle />
+    </div>
+  );
+}
