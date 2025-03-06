7cebb9e74d055b0b8349fc8e89dbef060dd84765
anis-6883 <anisseam238@gmail.com>
2025-03-06 19:56:05 +0600

    ci: authorize admin check

--- src/app/(client)/(auth)/secret-root/admin/login/page.tsx
+++ src/app/(client)/(auth)/secret-root/admin/login/page.tsx
@@ -1,11 +1,21 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/auth-options";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import { ROLE } from "@/config/constants";
 import { routes } from "@/config/routes";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import { IoHome } from "react-icons/io5";
 import AdminLoginForm from "./components/AdminLoginForm";
 
-export default function Page() {
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+
+  if (session && session?.user?.role === ROLE.ADMIN) {
+    redirect(routes.privateRoutes.admin.dashboard);
+  }
+
   return (
     <div className='flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10'>
       <div className='w-full max-w-sm md:max-w-xl'>
